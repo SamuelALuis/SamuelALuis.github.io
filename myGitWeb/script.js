cbf4142ec9e1938b59dfe9340d14d7ae8e4cfe0e
SamuelALuis <124638668+SamuelALuis@users.noreply.github.com>
2023-04-14 15:22:41 -0700

    update index

--- myGitWeb/script.js
+++ myGitWeb/script.js
@@ -1,8 +1,8 @@
 let workHisList = ["Freelance Graphic Designing", "Freelance Web Designing"]
 let skillList = ["HTML", "CSS", "JavaScript"]
-let goalsList = ["Learn efectivley the process of developing websites",
+let goalsList = ["Learn efectivley the process of developing websites.",
 "Creat websites that can be used by anyone across the world.",
-"Learn as many languages as I can"]
+"Learn as many languages as I can."]
 
 let workHisEl = document.getElementById("workhis-el")
 let skillsEl = document.getElementById("skills-el")
